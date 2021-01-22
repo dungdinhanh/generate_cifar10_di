0fee8c1f07dd15e25b822cc1b110b27a27910c9d
Dinh Anh Dung <dinhanhdung1996@gmail.com>
2021-01-22 15:58:13 +0800

    save change :

---
+++ scripts/res50gen.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+batch=256
+net="resnet50"
+for (( c=0; c<=1000; c++ ))
+do
+  cmd="python deepinversion_cifar10.py --teacher_weights checkpoint/state_dicts/${net}.pt --net ${net} --i_start ${c} --iters_mi 2001"
+  echo $cmd
+  eval $cmd
+  num_images=$((batch * c + batch))
+  echo $num_images
+  if [ $num_images -ge 50000 ]; then
+      break
+      fi
+done
+#cmd="python deepinversion_cifar10.py --teacher_weights checkpoint/state_dicts/resnet34.pt --net resnet34"
+#echo $cmd
+#eval $cmd
